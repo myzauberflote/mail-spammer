e99dde0f8413b8ac3badb0f40819dc1b47741143
myzauberflote <43866769+myzauberflote@users.noreply.github.com>
2018-10-04 20:59:10 -0300

    Adding comments

--- spammer2.py
+++ spammer2.py
@@ -2,35 +2,38 @@ import smtplib
 import random
 from time import sleep
 
-toadress = str(input('Digite o destinatário: '))
+toadress = str(input('Digite o destinatário: ')) # Type e-mail adress that we want to receive the message
 
-dicio = {}
+dicio = {} # Initialize dict that will store the mail address as key and password as value
 
 message = '''
-Oi.
-Eu quero cu.
+Type your message here!
+You can type multiple lines.
 '''
 
 with open('emails.txt') as f:
-    lista = f.readlines()
+    lista = f.readlines() # File reading and list creation containing all the line infos
 
 for string in lista:
+    '''
+    Here we separate mail adress and password in the string and store them on the dict
+    '''
     pos_virgula = string.find(',')
     email = string[:pos_virgula]
     senha = string[pos_virgula+1:].replace('\n','')
     dicio[email] = senha
 
 while True:
-    fromadress, password = random.choice(list(dicio.items()))
+    fromadress, password = random.choice(list(dicio.items())) # Inside the loop, the random choice from the dict
     username = fromadress
 
-    server = smtplib.SMTP('smtp.gmail.com',587)
+    server = smtplib.SMTP('smtp.gmail.com',587) # Starting connection to gmail server
     server.ehlo()
     server.starttls()
-    server.login(username,password)
+    server.login(username,password) # Login with username and password
 
     server.sendmail(fromadress,toadress,message)
     print('Mail sent!')
-    sleep(2)
+    sleep(1) # One second wait before another sending
 
 server.quit()
